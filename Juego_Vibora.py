ff16cf65085ec9334535abef1cbcb4d0cdbbdb38
valdezae <A00829909@itesm.mx>
2021-03-17 23:43:25 -0600

    Documentación código original
    Si se aplica este commit, se reflejará la documentación del código original

--- Juego_Vibora.py
+++ Juego_Vibora.py
@@ -2,9 +2,9 @@ from turtle import *
 from random import randrange
 from freegames import square, vector
 
-food = vector(0, 0)
-snake = [vector(10, 0)]
-aim = vector(0, -10)
+food = vector(0, 0) #Crea vector de food
+snake = [vector(10, 0)] #Crea vector de snake
+aim = vector(0, -10) #Crea vector de aim
 
 colorfood = randrange(1,6)
 colorsnake = randrange(1,6)
@@ -34,14 +34,14 @@ def change(x, y):
 
 def inside(head):
     "Return True if head inside boundaries."
-    return -200 < head.x < 190 and -200 < head.y < 190
+    return -200 < head.x < 190 and -200 < head.y < 190 #Límites de la ventana
 
 def move():
     "Move snake forward one segment."
     head = snake[-1].copy()
     head.move(aim)
 
-    if not inside(head) or head in snake:
+    if not inside(head) or head in snake: #Si la cabeza toca el cuerpo de la serpiente o toca los bordes de la ventana acaba el juego
         square(head.x, head.y, 9, 'red')
         update()
         return
@@ -49,9 +49,9 @@ def move():
     snake.append(head)
 
     if head == food:
-        print('Snake:', len(snake))
-        food.x = randrange(-15, 15) * 10
-        food.y = randrange(-15, 15) * 10
+        print('Snake:', len(snake)) #Muestra el tamaño actual de la serpiente
+        food.x = randrange(-15, 15) * 10 #Da un valor de x random de -15 a 15 para la posición de la comida
+        food.y = randrange(-15, 15) * 10 #Da un valor de y random de -15 a 15 para la posición de la comida
     else:
         snake.pop(0)
 
@@ -65,9 +65,9 @@ def move():
     ontimer(move, 100)
 
 setup(420, 420, 370, 0)
-hideturtle()
+hideturtle() #Esconde el cursor de turtle
 tracer(False)
-listen()
+listen() #Escucha el input del teclado
 onkey(lambda: change(10, 0), 'Right')
 onkey(lambda: change(-10, 0), 'Left')
 onkey(lambda: change(0, 10), 'Up')
